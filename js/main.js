15df3d52c52637233342e1e55fd751329352a1f1
Riley Bergamasco <rlbergamasco@gmail.com>
2021-10-30 16:58:24 -0400

    Added choropleth map and slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-let year = 2016;
+var year = 2002;
+var mapData = {};
+var testing = {};
 
 $(document).ready(function () {
     console.log("ready");
@@ -7,13 +9,28 @@ $(document).ready(function () {
 });
 
 function loadData() {
-    $.getJSON("data/energy-data.json", function (data) {
+    $.getJSON("data/energy.json", function (data) {
         parseData(data);
     });
 }
 
 function parseData(data) {
-    // filtered = data.filter(item => item.year === year);
+    console.log(data);
+    // let newData = [];
+    // $.each(data, function (i) {
+    //     let current = newData.find(element => element.iso === data[i].iso);
+    //     if (current === undefined) {
+    //         newData.push({
+    //             "iso": data[i].iso,
+    //             "country": data[i].country,
+    //             "years": {}
+    //         })
+    //     }
+    //     current = newData.find(element => element.iso === data[i].iso);
+    //     current.years[data[i].year] = [data[i].year, data[i].population, data[i].consumption];
+    // });
+    // console.log(newData);
+
     const htmlHeader = `<thead><tr>
                         <th>Year</th>
                         <th>Country</th>
@@ -21,29 +38,92 @@ function parseData(data) {
                         <th>Primary Energy Consumption (TWh)</th>
                         <th>Consumption per Capita (MWh)</th>
                         </tr></thead>`;
-    let htmlBody = `<tbody>`;
+    let htmlBody = `<tbody>`
+    $.each(data, function (i) {
+        if (data[i].years[year] != undefined) {
+            let tempCapita = data[i].years[year][2] / data[i].years[year][1] * 1000000;
+            tempCapita = tempCapita.toFixed(3)
+            let key = "Unknown";
+            if (tempCapita < 40) {
+                key = "0-39";
+            } else if (tempCapita < 60) {
+                key = "40-59";
+            } else if (tempCapita < 80) {
+                key = "60-79";
+            } else if (tempCapita < 100) {
+                key = "80-99";
+            } else if (tempCapita < 120) {
+                key = "100-119";
+            } else {
+                key = "120+";
+            }
+            mapData[data[i].iso] = {
+                fillKey: key,
+                count: tempCapita
+            };
+            testing["USA"] = {
+                fillKey: "0-59",
+                count: 12
+            };
+        }
 
-    $.each(data, function (index) {
-        htmlBody += `<tr>`;
-        htmlBody += `<td>${data[index].year}</td>`;
-        htmlBody += `<td>${data[index].country}</td>`;
-        htmlBody += `<td>${data[index].population}</td>`;
-        htmlBody += `<td>${data[index].consumption}</td>`;
-        let percapita = data[index].consumption / data[index].population * 1000000;
-        percapita = percapita.toFixed(3)
-        percapita = data[index].consumption === "" ? "" : percapita
-        htmlBody += `<td>${percapita}</td>`;
-        htmlBody += `</tr>`;
+        $.each(data[i].years, function (j) {
+            let current = data[i].years[j];
+            htmlBody += `<tr>`;
+            htmlBody += `<td>${current[0]}</td>`;
+            htmlBody += `<td>${data[i].country}</td>`;
+            htmlBody += `<td>${current[1]}</td>`;
+            htmlBody += `<td>${current[2]}</td>`;
+            let percapita = current[2] / current[1] * 1000000;
+            percapita = percapita.toFixed(3)
+            htmlBody += `<td>${percapita}</td>`;
+            htmlBody += `</tr>`;
+        });
     });
+    console.log(mapData)
     htmlBody += `</tbody>`;
-
-
     $('#my-table').append(htmlHeader);
     $('#my-table').append(htmlBody);
     buildTable();
+    buildMap();
 }
 
 function buildTable() {
     $('#my-table').DataTable();
+    console.log(testing)
     console.log("done")
 }
+
+function buildMap() {
+    var map = new Datamap({
+        element: document.getElementById('map'),
+        responsive: true,
+        fills: {
+            Unknown: '#d6d6d6',
+            "0-39": '#bfdcf1',
+            "40-59": '#95CFF9',
+            "60-79": '#5fb9fa',
+            "80-99": '#4da4e1',
+            "100-119": '#3C8FC9',
+            "120+": '#2b6690',
+            defaultFill: '#d6d6d6'
+        },
+        // data: testing,
+        data: mapData,
+        geographyConfig: {
+            popupTemplate: function (geo, data) {
+                return ['<div class="hoverinfo">',
+                    geo.properties.name + ": <strong>",
+                    + data.count,
+                    '</strong></div>'].join('');
+            }
+        }
+    });
+
+    map.legend();
+}
+
+
+window.addEventListener("resize", function () {
+    map.resize();
+});
